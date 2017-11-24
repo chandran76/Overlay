2394e118221a93af8ad83137a67f6ebe9ad0a805
chandran76 <33481918+chandran76@users.noreply.github.com>
2017-11-24 15:19:32 +0530

    Add files via upload

---
+++ scripts/GetContextLabelAttributesArg.js
@@ -0,0 +1,27 @@
+﻿//Context Label Attributes arg class
+function GetContextLabelAttributesArg() {
+    var thisArg = this;
+    var contextLabelId;
+    var results;
+
+    Object.defineProperty(this, "ContextLabelId", {
+        get: function () {
+            return (thisArg.contextLabelId);
+        },
+
+        set: function (value) {
+            thisArg.contextLabelId = value;
+        }
+    });
+    
+    Object.defineProperty(this, "Results", {
+        get: function () {
+            return (thisArg.results);
+        },
+
+        set: function (value) {
+            thisArg.results = value;
+        }
+    });
+
+}
